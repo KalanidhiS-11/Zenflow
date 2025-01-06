4393c62e0b0db302701a599674152a5428b12d60
KalanidhiS-11 <dhayanidhis93@gmail.com>
2025-01-06 16:38:41 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,23 +1,13 @@
-"""
-URL configuration for zenfloe project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, ProjectViewSet, TaskViewSet, InvoiceViewSet
+
+router = DefaultRouter()
+router.register('users', UserViewSet)
+router.register('projects', ProjectViewSet)
+router.register('tasks', TaskViewSet)
+router.register('invoices', InvoiceViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/',include('core.urls')),
+    path('', include(router.urls)),
 ]
